c9194c4da6e8e843685b1b70e328f6db5a04fd45
Dmitriy-hello-world <easportstng@gmail.com>
2024-04-20 14:55:33 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@mui/material';
+import AbcIcon from '@mui/icons-material/Abc';
+function App() {
+  return (
+    <>
+      <AbcIcon />
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </>
+  );
+}
+
+export default App;
